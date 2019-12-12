1a59846aa4a1236a56bcd7e74c3d5aad4baad45b
pnouel <petronouel@gmail.com>
2019-12-12 00:42:45 -0300

    Cambios Footer hasta donde me dio la vida

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,25 +1,27 @@
-<footer class= "bg-dark foot p-3">
+<footer class= "foot p-3 footLayout">
     <div class="row mx-0">
-      <div class=" col-12 col-md-4">
-        <h3 class="foot">Lavadero Lava Bien </h3>
+      <div class=" col-12 col-md-4 border-right border-white">
+        <h4 class="foot">Informacion de interes</h4>
+        <div>
           <ul style="list-style:none">
-            <li ><a class ="nav-link" href="#">Contacto</a> </li>
-            <li><a class ="nav-link" href="#">Institucional</a> </li>
-            <li><a class ="nav-link"  href="#">Sobre Nosotros</a> </li>
+            <li ><a class ="nav-link text-left" href="#">Sobre Nosotros</a></li>
+            <li><a class ="nav-link text-left" href="#">Preguntas Frecuentes</a> </li>
+            <li><a class ="nav-link text-left"  href="#">Contactanos</a></li>
           </ul>
+        </div>
+
+        </div>
+        <div class="col-12 col-md-4 border-right border-white">
+          <h4  class="foot"><h4 class="foot">Servicios</h4></h4>
           <p class="iconos">
-            <i class="fab fa-whatsapp"><a href="http://wa.me/541130847961?text=hola queria realizar el siguiente pedido "></a></i>
-            <i class="fab fa-facebook"><a href="https://www.facebook.com/Aurora-Materiales-1531611110225827/"></a></i>
-            <i class="fab fa-instagram"></i>
-            <i class="fab fa-youtube"><a href="https://www.youtube.com/"></a></i>
-          </p>
+                <i class="fab fa-whatsapp"><a href="http://wa.me/541130847961?text=hola queria realizar el siguiente pedido "></a></i>
+                <i class="fab fa-facebook"><a href="https://www.facebook.com/Aurora-Materiales-1531611110225827/"></a></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-youtube"><a href="https://www.youtube.com/"></a></i>
+              </p>
         </div>
-        <div class="col-12 col-md-4">
-          <h3  class="foot">Direccion</h3>
-          <p  class="foot">Monroe 800</p>
-          <p  class="foot">Tel 4444-4444 / 5555-5555</p>
-        </div>
-        <div class="d-none d-md-block col-md-4">
+        <div class="d-none d-md-block col-md-4 ">
+                <h4  class="foot"><h4 class="foot">Ubicacion</h4></h4>
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13144.745803702659!2d-58.4436762!3d-34.5488343!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfb39818e7624ac76!2sDigital%20House!5e0!3m2!1ses-419!2sar!4v1567093583583!5m2!1ses-419!2sar" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
           <hr>

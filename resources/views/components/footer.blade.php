6a423e82d4fa809e042245f3f7794c1546f321c7
pnouel <petronouel@gmail.com>
2019-12-13 02:02:39 -0300

    Redes sociales y ubicacion

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,26 @@
 <footer class= "footFont p-3 footLayout">
     <div class="row mx-0">
-      <div class=" col-12 col-md-4 border-right border-white  footFont">
+      <div class="col-lg-4 col-md-4 col-sm-12 border-right border-white  footFont">
         <h4 class="footFont"><strong>Informacion de interes</strong></h4>
         <div>
           <ul style="list-style:none" class="footFont">
             <li ><a class ="nav-link text-left ml-5" href="{{ route('register') }}">Registrate</a></li>
-            <li><a class ="nav-link text-left ml-5" href="{{asset("FAQ")}}">Preguntas Frecuentes</a> </li>
-            <li><a class ="nav-link text-left ml-5 "  href="{{asset("contact")}}"></a>Contacto</li>
+            <li><a class ="nav-link text-left ml-5" href="{{asset("FAQ")}}">Preguntas Frecuentes</a></li>
+            <li><a class ="nav-link text-left ml-5 "  href="{{asset("contact")}}">Contacto</a></li>
           </ul>
         </div>
 
         </div>
-        <div class="col-12 col-md-4 border-right border-white">
-          <h4 class="footFont"><strong> Servicios</strong></h4>
-          <p class="iconos">
-                <i class="fab fa-whatsapp"><a href="http://wa.me/541130847961?text=hola queria realizar el siguiente pedido "></a></i>
-                <i class="fab fa-facebook"><a href="https://www.facebook.com/Aurora-Materiales-1531611110225827/"></a></i>
-                <i class="fab fa-instagram"></i>
-                <i class="fab fa-youtube"><a href="https://www.youtube.com/"></a></i>
+        <div class="col-lg-4 col-md-4 col-sm-12 border-right border-white">
+          <h4 class="footFont"><strong>Nuestras Redes Sociales</strong></h4>
+          <p class="iconos mt-4">
+                <a href="http://wa.me/541130847961?text=hola queria realizar el siguiente pedido "><i class="fab fa-whatsapp"></i></a>
+                <a href="https://www.facebook.com/lavadero.lavabien.79"><i class="fab fa-facebook"></i></a>
+                <a href="https://www.instagram.com/?hl=es-la"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
               </p>
         </div>
-        <div class="d-none d-md-block col-md-4 ">
+        <div class="col-lg-4 col-md-4 col-sm-12 d-none d-md-block google-maps ">
                 <h4 class="footFont"><strong>Ubicacion</strong></h4>
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13144.745803702659!2d-58.4436762!3d-34.5488343!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfb39818e7624ac76!2sDigital%20House!5e0!3m2!1ses-419!2sar!4v1567093583583!5m2!1ses-419!2sar" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>

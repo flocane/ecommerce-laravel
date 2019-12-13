ff6173b330c7fb1a3ed03ed59d5f68519a08d057
pnouel <petronouel@gmail.com>
2019-12-13 00:12:44 -0300

    Cambio de FontColor y eliminacion de etiquetas duplicadas

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,9 +1,9 @@
-<footer class= "foot p-3 footLayout">
+<footer class= "footFont p-3 footLayout">
     <div class="row mx-0">
-      <div class=" col-12 col-md-4 border-right border-white">
-        <h4 class="foot">Informacion de interes</h4>
+      <div class=" col-12 col-md-4 border-right border-white  footFont">
+        <h4 class="footFont"><strong>Informacion de interes</strong></h4>
         <div>
-          <ul style="list-style:none">
+          <ul style="list-style:none" class="footFont">
             <li ><a class ="nav-link text-left" href="#">Sobre Nosotros</a></li>
             <li><a class ="nav-link text-left" href="#">Preguntas Frecuentes</a> </li>
             <li><a class ="nav-link text-left"  href="#">Contactanos</a></li>
@@ -12,7 +12,7 @@
 
         </div>
         <div class="col-12 col-md-4 border-right border-white">
-          <h4  class="foot"><h4 class="foot">Servicios</h4></h4>
+          <h4 class="footFont"><strong> Servicios</strong></h4>
           <p class="iconos">
                 <i class="fab fa-whatsapp"><a href="http://wa.me/541130847961?text=hola queria realizar el siguiente pedido "></a></i>
                 <i class="fab fa-facebook"><a href="https://www.facebook.com/Aurora-Materiales-1531611110225827/"></a></i>
@@ -21,7 +21,7 @@
               </p>
         </div>
         <div class="d-none d-md-block col-md-4 ">
-                <h4  class="foot"><h4 class="foot">Ubicacion</h4></h4>
+                <h4 class="footFont"><strong>Ubicacion</strong></h4>
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13144.745803702659!2d-58.4436762!3d-34.5488343!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xfb39818e7624ac76!2sDigital%20House!5e0!3m2!1ses-419!2sar!4v1567093583583!5m2!1ses-419!2sar" width="300" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
         </div>
           <hr>

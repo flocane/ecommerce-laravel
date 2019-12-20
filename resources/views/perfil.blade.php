4afc2df3802e457a074b629516b8906abe4e1ccb
Gise <sanchezgisela85@gmail.com>
2019-12-20 00:18:29 -0300

    todo ok

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -18,12 +18,7 @@
         </div>
 
         <p class='gssnombre'>{{Auth::user()->name}} {{Auth::user()->lastname}}</p>
-<<<<<<< .merge_file_jggL2Y
        <button type="button" class='btn btn-outline-secondary boton'> <a href="{{'/users/'.Auth::user()->id.'/edit/'}}">Editar Datos</a>
-=======
-        <button type = 'submit' class='btn btn-outline-secondary boton'>actualizar mi foto de perfil</button>
-        <button type="button" class='btn btn-outline-secondary boton'> <a href="{{'/users/'.Auth::user()->id.'/edit/'}}">Editar Datos</a>
->>>>>>> .merge_file_Mx89BF
 
     </div>
 
